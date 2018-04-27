4e8be3bf17fee4536d37f98057f13f8d965ad00d
Auston Stewart <auston@shedbuilt.net>
2018-04-26 16:48:45 -1000

    Initial commit of slang

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+./configure --prefix=/usr \
+            --sysconfdir=/etc \
+            --disable-static \
+            --with-readline=gnu &&
+make -j1 &&
+make DESTDIR="$SHED_FAKEROOT" \
+     install_doc_dir=/usr/share/doc/slang-2.3.2 \
+     SLSH_DOC_DIR=/usr/share/doc/slang-2.3.2/slsh \
+     install &&
+chmod -v 755 "${SHED_FAKEROOT}/usr/lib/libslang.so.2.3.2" \
+             "${SHED_FAKEROOT}"/usr/lib/slang/v2/modules/*.so
